f18c25898df3c2ccff0894c82f5bd8870d6103cb
zhuwt <405088254@qq.com>
2016-09-07 21:54:20 +0800

    Finished detail page
    Finished detail page

--- config.js
+++ config.js
@@ -1,21 +1,6 @@
 /**
  * Created by Bucky.Zhu on 08/04/2016.
  */
-// angular.module('thisApp').
-// config(['$routeProvider',
-//     function config($routeProvider) {
-//         $routeProvider.
-//         when('/home', {
-//             template: 'src/home/home.html',
-//             controller:'homeController'
-//         }).
-//         // when('/phones/:phoneId', {
-//         //     template: '<phone-detail></phone-detail>'
-//         // }).
-//         otherwise('/home');
-//     }
-// ]);
-
 routeApp.config(['$routeProvider',function ($routeProvider) {
     console.log('enter the route function!');
     $routeProvider

ac24c5d4fee9a7ead5ad58b8aa966c0f60127178
zhuwt <405088254@qq.com>
2016-11-26 21:13:23 +0800

    update

--- src/model/orders/orders.js
+++ src/model/orders/orders.js
@@ -15,6 +15,30 @@ angular.module('thisApp.orders', [])
             orderService.getOrderList(accId,function (data) {
                 console.log(data);
                 vm.orders = data;
+                for (var n=0;n<vm.orders.length;n++){
+                    switch (vm.orders[n].orderState.toString()){
+                        case '0':
+                            vm.orders[n].orderState = '待支付';
+                            break;
+                        case '1':
+                            vm.orders[n].orderState = '已支付';
+                            break;
+                        case '2':
+                            vm.orders[n].orderState = '出货';
+                            break;
+                        case '3':
+                            vm.orders[n].orderState = '派送中';
+                            break;
+                        case '4':
+                            vm.orders[n].orderState = '已完成';
+                            break;
+                        case '5':
+                            vm.orders[n].orderState = '已关闭';
+                            break;
+                        default:
+                            break;
+                    }
+                }
             });
         };
         vm.init();
